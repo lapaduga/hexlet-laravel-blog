983d9683a4d08d1e497bb2ed494335080ba05143
lapaduga <deniskotov29042015@gmail.com>
2024-09-23 00:55:24 +0500

    lesson 14

--- resources/views/article/form.blade.php
+++ resources/views/article/form.blade.php
@@ -8,15 +8,7 @@
     </div>
 @endif
 
-{{ html()->modelForm($article, 'POST', route('articles.store'))->open() }}
 {{ html()->label('Имя', 'name') }}
 {{ html()->input('text', 'name') }}
 {{ html()->label('Содержание', 'body') }}
 {{ html()->textarea('body') }}
-{{ html()->submit('Создать') }}
-{{ html()->closeModelForm() }}
-
-{{ html()->modelForm($article, 'POST', route('articles.store'))->open() }}
-@include('article.form')
-{{ html()->submit('Создать') }}
-{{ html()->closeModelForm() }}
--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -3,7 +3,10 @@
 @section('content')
     <h1>Список статей</h1>
     @foreach ($articles as $article)
-        <h2><a href="{{ route('articles.show', $article->id) }}">{{$article->name}}</a></h2>
+        <h2>
+            <a href="{{ route('articles.show', $article->id) }}">{{$article->name}}</a>
+            <a href="{{ route('articles.edit', $article->id) }}">Edit</a>
+        </h2>
         {{-- Str::limit – функция-хелпер, которая обрезает текст до указанной длины --}}
         {{-- Используется для очень длинных текстов, которые нужно сократить --}}
         <div>{{Str::limit($article->body, 200)}}</div>
