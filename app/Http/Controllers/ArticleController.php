557798dcff0ddee83ee4ae1068c130335bc0bfc2
lapaduga <deniskotov29042015@gmail.com>
2024-09-22 23:00:22 +0500

    lesson 13

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,6 +7,14 @@ use App\Models\Article;
 
 class ArticleController extends Controller
 {
+    // Вывод формы
+    public function create()
+    {
+        // Передаем в шаблон вновь созданный объект. Он нужен для вывода формы
+        $article = new Article();
+        return view('article.create', compact('article'));
+    }
+
     public function index()
     {
         $articles = Article::paginate();
@@ -20,5 +28,27 @@ class ArticleController extends Controller
     {
         $article = Article::findOrFail($id);
         return view('article.show', compact('article'));
+    }
+
+    // Здесь нам понадобится объект запроса для извлечения данных
+    public function store(Request $request)
+    {
+        // Проверка введенных данных
+        // Если будут ошибки, то возникнет исключение
+        // Иначе возвращаются данные формы
+        $data = $request->validate([
+            'name' => 'required|unique:articles',
+            'body' => 'required|min:1000',
+        ]);
+
+        $article = new Article();
+        // Заполнение статьи данными из формы
+        $article->fill($data);
+        // При ошибках сохранения возникнет исключение
+        $article->save();
+
+        // Редирект на указанный маршрут
+        return redirect()
+            ->route('articles.index');
     }
 }
